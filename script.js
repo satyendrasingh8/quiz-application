632f529e1e90ea774236a72ce898ccb2d3095025
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2022-03-15 07:04:38 +0530

    style and dummy data

---
+++ script.js
@@ -0,0 +1,28 @@
+const quizDB =[
+    {
+question:"What is part of a database that holds only one type of information?",
+a:"report",
+b:"field",
+c:"record",
+d:"file",
+ans:"ans2"
+    },
+    {
+        question:"'OS' computer abbreviation usually means ?",
+        a:"Order of Significance",
+        b:"Open SOFTWARE",
+        c:"Operating System",
+        d:"Optical sensor",
+        ans:"ans3"
+            },
+
+            {
+                question:"'.MOV' extension refers usually to what kind of file?",
+                a:"Image file",
+                b:"Animation/movie file",
+                c:"Audio file",
+                d:"MS Office document",
+                ans:"ans2"
+                    },
+                   
+]
